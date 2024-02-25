b51d54d02102acdb0856128c0a365d2c2c4ae398
SHAHJEE <154551822+syedzubairalishah@users.noreply.github.com>
2024-02-24 22:39:26 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+let message1 = "HELLO USER";
+alert(message1);
+
+let num1 = prompt("ENTER YOUR FIRST NUMBER");
+let num2 = prompt("ENTER YOUR SECOND NUMBER");
+
+num1 = Number(num1);
+num2 = Number(num2);
+
+console.log("num1:", num1);
+console.log("num2:", num2);
+
+let sum = num1 + num2;
+let minus = num1 - num2;
+let multiply = num1 * num2;
+let divide = num1 / num2;
+
+alert("ADDITION=" + sum);
+alert("SUBTRACT=" + minus);
+alert("Multiplication=" + multiply);
+alert("Division=" + divide);
+
+let message2 = "THANKS USER";
+alert(message2);
